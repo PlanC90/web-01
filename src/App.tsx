af2827fbb22e954356d230c19e3a3b2f32623334
Morkolive <103614958+Morkolive@users.noreply.github.com>
2025-03-05 21:48:24 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -103,7 +103,7 @@ function App() {
 
           {/* Holder Count */}
           <div className="text-6xl font-extrabold text-blue-400 mb-8">
-            135,000+ Holders
+            140,000+ Holders
           </div>
 
           <div className="flex flex-col sm:flex-row gap-4 mb-16">
